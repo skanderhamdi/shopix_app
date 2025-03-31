ce2aedc33ab06b6c56c9a2095efe227c0531666a
Skander HAMDI <alexhamdi94@gmail.com>
2025-03-31 05:53:26 +0100

    Update API to server

--- lib/Helper/constants.dart
+++ lib/Helper/constants.dart
@@ -1,7 +1,7 @@
 
 class Constants {
   static String appLogo = 'assets/logo/app-logo.png';
-  static String baseURL = 'http://172.20.10.4:8000/api';
+  static String baseURL = 'https://s-hamdi.com/flutter_course/public/api/';
   static String contentType = 'application/json';
   static Map<String, String> headers = {
     'Accept': 'application/json',
